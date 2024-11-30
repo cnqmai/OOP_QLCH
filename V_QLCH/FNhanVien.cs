04b848647d24f561ed3c63b501a8dfea49aa4904
cnqmai <165132013+cnqmai@users.noreply.github.com>
2024-11-30 23:19:31 +0700

    Cấu trúc chung

---
+++ V_QLCH/FNhanVien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHangT1S.V_QLCH
+{
+    public partial class FNhanVien : Form
+    {
+        public FNhanVien()
+        {
+            InitializeComponent();
+        }
+    }
+}
